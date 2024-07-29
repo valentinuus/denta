5deb6deb3f58b989520d6db20ff046d1abd58216
Valentin <72602932+valentinuus@users.noreply.github.com>
2024-07-29 20:14:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -59,26 +59,28 @@ testWebP(function (support) {
 
 
 //menu
-const burger = document?.querySelector('[data-burger]');
-const nav = document?.querySelector('[data-nav]');
-const navItems = nav?.querySelectorAll('a');
+const burger = document.querySelector('.burger');
+const burgerClose = document.querySelector('.burger_close');
+const nav = document.querySelector('.header__nav');
+const navItems = nav.querySelectorAll('.nav__link');
 const body = document.body;
-const header = document?.querySelector('.header');
-const headerHeight = header.offsetHeight;
-// console.log(headerHeight)
-document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
+const header = document.querySelector('.header');
 
-burger?.addEventListener('click', () => {
-	body.classList.toggle('stop-scroll');
-	burger?.classList.toggle('burger--active');
-	nav?.classList.toggle('nav--visible');
+burger.addEventListener('click', () => {
+	body.classList.add('stop-scroll');
+	nav.classList.add('nav--visible');
+});
+
+burgerClose.addEventListener('click', () => {
+	body.classList.remove('stop-scroll');
+	nav.classList.remove('nav--visible');
 });
 
 navItems.forEach(el => {
 	el.addEventListener('click', () => {
 		body.classList.remove('stop-scroll');
-		burger?.classList.remove('burger--active');
-		nav?.classList.remove('nav--visible');
+		burger.classList.remove('burger--active');
+		nav.classList.remove('nav--visible');
 	});
 });
 
